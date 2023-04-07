4a1355f4081952f542660dbd917da1b0bd6d4671
Angus Ryer <angusryer@gmail.com>
2023-04-07 16:15:09 -0400

    ENG-20 Impl audio stream opening in C++

--- cpp/react-native-audio-cortex.cpp
+++ cpp/react-native-audio-cortex.cpp
@@ -1,7 +1,57 @@
 #include "react-native-audio-cortex.h"
+#include <oboe/Oboe.h>
 
-namespace audiocortex {
-	double multiply(double a, double b) {
+namespace audiocortex
+{
+	double multiply(double a, double b)
+	{
 		return a * b;
 	}
+
+	class AudioInputCallback : public oboe::AudioStreamCallback
+	{
+	public:
+		oboe::DataCallbackResult onAudioReady(oboe::AudioStream *oboeStream, void *audioData, int32_t numFrames) override
+		{
+			// Process the audio data here
+			return oboe::DataCallbackResult::Continue;
+		}
+
+		void onErrorBeforeClose(oboe::AudioStream *oboeStream, oboe::Result error) override
+		{
+			// Handle errors here
+		}
+
+		void onErrorAfterClose(oboe::AudioStream *oboeStream, oboe::Result error) override
+		{
+			// Handle errors here
+		}
+	};
+
+	std::unique_ptr<oboe::AudioStream> audioStream;
+	AudioInputCallback audioInputCallback;
+
+	void setupAudioStream()
+	{
+		oboe::AudioStreamBuilder builder;
+		builder.setDirection(oboe::Direction::Input);
+		builder.setPerformanceMode(oboe::PerformanceMode::LowLatency);
+		builder.setInputPreset(oboe::InputPreset::VoiceRecognition);
+		builder.setSampleRate(48000);
+		builder.setChannelCount(1);
+		builder.setFormat(oboe::AudioFormat::Float);
+		builder.setCallback(&audioInputCallback);
+
+        oboe::AudioStream *tempStream;
+        oboe::Result result = builder.openStream(&tempStream);
+        if (result != oboe::Result::OK) {
+            // Handle errors
+            return;
+        }
+        audioStream.reset(tempStream);
+
+		// Start the audio stream
+		audioStream->requestStart();
+	}
+
 }
